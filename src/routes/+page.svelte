8752c195166a2872ea644473f9b1aea37c60a156
GnotAGnoob <danisvatek@gmail.com>
2023-08-06 21:22:19 +0200

    title setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<script>
+<script lang="ts">
     import { Button } from '@svelteuidev/core';
+
+    export let data;
+
+    const x = {x:1}
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit {x.x}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Button>ahoj</Button>
+<Button>{data.test}</Button>

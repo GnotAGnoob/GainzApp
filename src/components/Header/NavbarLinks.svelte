6d35ee3b79c77fea23d2a8a0a00174321a643199
GnotAGnoob <danisvatek@gmail.com>
2023-08-08 13:02:47 +0200

    opraveni eslintu, pridani hlavicky, css reset 2

---
+++ src/components/Header/NavbarLinks.svelte
@@ -0,0 +1,11 @@
+<script>
+	import NavbarLink from "./NavbarLink.svelte";
+</script>
+
+<nav>
+	<ul>
+		<NavbarLink href="/" text="Home" />
+		<NavbarLink href="/create" text="Create" />
+		<NavbarLink href="/list" text="List" />
+	</ul>
+</nav>

2d6f4aa2f222c93040b85d9c2d64b05ff5765bbb
GnotAGnoob <danisvatek@gmail.com>
2023-08-31 01:06:04 +0200

    devmode warning2

---
+++ src/components/EnvironmentBanner.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let type: "positive" | "info" = "positive";
+</script>
+
+<h2 class="banner banner_{type}">
+	<slot />
+</h2>
+
+<style lang="scss">
+	.banner {
+		width: 100%;
+		padding: $space-sm $space-sm;
+
+		text-align: center;
+		font-size: $text;
+		font-weight: 500;
+
+		background-color: var(--accent-positive-500);
+
+		&_info {
+			background-color: var(--accent-info-500);
+		}
+	}
+</style>

071cb90635696fc053cc3033a6b10d8c2a912ce3
GnotAGnoob <danisvatek@gmail.com>
2023-11-08 23:39:52 +0100

    templating before fill workout implementation2

---
+++ src/components/Workouts/Superset/DisplaySuperset.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import Exercise from "../Exercise.svelte";
+	import type { PageExercise } from "$src/routes/workouts/types";
+
+	export let exercises: PageExercise[] = [];
+	export let order: number;
+</script>
+
+<div class="superset">
+	<h5 class="title">{order}. {exercises.length > 1 ? "superset" : "exercise"}</h5>
+	<div class="exercises">
+		{#each exercises as exercise}
+			<Exercise {exercise} />
+		{/each}
+	</div>
+</div>
+
+<style lang="scss">
+	.exercises {
+		display: flex;
+
+		flex-direction: column;
+
+		line-height: 1;
+	}
+
+	.title {
+		font-size: $text-tag;
+		font-weight: 700;
+		color: var(--text-secondary);
+	}
+
+	.superset {
+		display: flex;
+
+		gap: $space-xxs;
+
+		flex-direction: column;
+
+		padding-right: ($space-md + $space-xs);
+
+		color: var(--accent-neutral-700);
+	}
+</style>
---
+++ src/components/Workouts/Superset/EditableSuperset.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import EditableExercise from "../EditableExercise.svelte";
+	import type { PageExercise } from "$src/routes/workouts/types";
+	import SupersetTemplate from "./SupersetTemplate.svelte";
+
+	export let exercises: PageExercise[] = [];
+	export let order: number;
+
+	const onDelete = (index: number) => {
+		exercises = exercises.filter((_, i) => i !== index);
+	};
+</script>
+
+<SupersetTemplate {order} bind:exercises>
+	{#each exercises as exercise, index}
+		<EditableExercise bind:exercise onDelete={() => onDelete(index)} />
+	{/each}
+</SupersetTemplate>

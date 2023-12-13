aa87d223a695799ab2276d28303d66ef1f3c8696
GnotAGnoob <danisvatek@gmail.com>
2023-12-14 00:43:28 +0100

    doing some todos2

---
+++ src/components/Atoms/EmptyCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let text: string;
+</script>
+
+<h4 class="empty">
+	{text}
+</h4>
+
+<style lang="scss">
+	.empty {
+		margin: $space-md auto $space-sm;
+		padding: $space-lg $space-lg;
+		border-radius: $border-sm;
+
+		justify-content: center;
+
+		font-weight: 700;
+		background-color: var(--accent-neutral-100);
+	}
+</style>

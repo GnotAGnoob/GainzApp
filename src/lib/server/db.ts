737e859700be738485a9164d009af9e825e842a8
GnotAGnoob <danisvatek@gmail.com>
2024-03-24 18:26:19 +0100

    workflows

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,4 +1,4 @@
-import { DATABASE_URL } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
 import { neon, neonConfig } from "@neondatabase/serverless";
@@ -8,20 +8,20 @@ import schema from "$db/schema";
 import { envError } from "../error";
 import type { Database } from "./dbTypes";
 
-if (!DATABASE_URL) throw envError("DATABASE_URL");
+if (!env.DATABASE_URL) throw envError("DATABASE_URL");
 
 let db: Database;
 neonConfig.fetchConnectionCache = true;
 
 if (import.meta.env.MODE === "development") {
 	if (!global._db) {
-		const queryClient = postgres(DATABASE_URL, { max: 1 });
+		const queryClient = postgres(env.DATABASE_URL, { max: 1 });
 		global._db = drizzle(queryClient, { schema });
 	}
 
 	db = global._db;
 } else {
-	const sql = neon(DATABASE_URL);
+	const sql = neon(env.DATABASE_URL);
 	db = neonDrizzle(sql, { schema });
 }
 

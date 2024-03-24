3f84d823b2d1ade4ad257d1d559cdde6cfcdec95
GnotAGnoob <danisvatek@gmail.com>
2024-03-24 21:11:23 +0100

    fix build

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,4 @@
 import { env } from "$env/dynamic/private";
-import { NODE_ENV } from "$env/static/private";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
 import { neon, neonConfig } from "@neondatabase/serverless";
@@ -9,27 +8,21 @@ import schema from "$db/schema";
 import { envError } from "../error";
 import type { Database } from "./dbTypes";
 
-let db: Database = {} as Database;
+let db: Database;
 
-const setDB = () => {
-	if (NODE_ENV === "ci") return;
+if (!env.DATABASE_URL) throw envError("DATABASE_URL");
 
-	if (!env.DATABASE_URL) throw envError("DATABASE_URL");
-	neonConfig.fetchConnectionCache = true;
+neonConfig.fetchConnectionCache = true;
 
-	if (!global._db) {
-		if (import.meta.env.MODE === "development") {
-			const queryClient = postgres(env.DATABASE_URL, { max: 1 });
-			global._db = drizzle(queryClient, { schema });
-		} else {
-			const sql = neon(env.DATABASE_URL);
-			global._db = neonDrizzle(sql, { schema });
-		}
+if (!global._db) {
+	if (import.meta.env.MODE === "development") {
+		const queryClient = postgres(env.DATABASE_URL, { max: 1 });
+		global._db = drizzle(queryClient, { schema });
+	} else {
+		const sql = neon(env.DATABASE_URL);
+		global._db = neonDrizzle(sql, { schema });
 	}
-
-	db = global._db;
-};
-
-setDB();
+}
+db = global._db;
 
 export default db;

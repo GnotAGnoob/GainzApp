803f73fde5e989ed8a5bfbbaebba267325862c7f
GnotAGnoob <danisvatek@gmail.com>
2024-04-07 23:06:02 +0200

    fix type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,10 +8,10 @@ export type StylesType =
 	| "background"
 	| "inherit";
 
-type InputEventType<E, T extends EventTarget> = E & {
+type InputEventTemplate<E, T extends EventTarget> = E & {
 	currentTarget: EventTarget & T;
 };
 
-export type KeyboardEventType<T extends EventTarget> = InputEventType<KeyboardEvent, T>;
-export type ClipboardEventType<T extends EventTarget> = InputEventType<ClipboardEvent, T>;
-export type InputEventType<T extends EventTarget> = InputEventType<InputEvent, T>;
+export type KeyboardEventType<T extends EventTarget> = InputEventTemplate<KeyboardEvent, T>;
+export type ClipboardEventType<T extends EventTarget> = InputEventTemplate<ClipboardEvent, T>;
+export type InputEventType<T extends EventTarget> = InputEventTemplate<InputEvent, T>;
